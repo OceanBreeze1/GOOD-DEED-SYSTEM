80934463c8d2fa71b4c9eb405c4b1f41ee712549
Sanam2002 <104547253+Sanam2002@users.noreply.github.com>
2022-05-06 12:51:50 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,87 @@
-var jtext='{"Images":[' +
-          '{"URL":"img/01.jpg","Caption":"Image 1"},'+
-          '{"URL":"img/02.jpg","Caption":"Image 2"},'+
-          '{"URL":"img/03.jpg","Caption":"Image 3"},'+
-          '{"URL":"img/04.jpg","Caption":"Image 4"},'+
-          '{"URL":"img/05.jpg","Caption":"Image 5"},'+
-          '{"URL":"img/06.jpg","Caption":"Image 6"},'+
-          '{"URL":"img/07.jpg","Caption":"Image 7"},'+
-          '{"URL":"img/08.jpg","Caption":"Image 8"},'+
-          '{"URL":"img/09.jpg","Caption":"Image 9"}'+
-          ']}';
-var json = JSON.parse(jtext);
-var content =document.getElementById('content');
-var heading =document.createElement('h1');//<h1.this is a gallery</h1>
-heading.innerText ="This is the gallery page";
-content.appendChild(heading);
-var gallery = document.getElementById('gallery');
-for(var i=0; i<json.Images.length ; i++){
-var divCol = document.createElement('div');
-divCol.class ="card";
-var image =document.createElement('img');
-var divCard = document.createElement('div');
+fetch('json/images.json').then(function(response) {
+  response.json().then(function(json) {
 
-divCard.class="card";
-divCard.style="width:30%";
-var divCardBody = document.createElement('div');
-divCardBody.class = "card-body";
-var caption = document.createElement('h4');
-caption.innerText=json.Images[i].Caption;
-caption.style="text-align:center";
-image.src =json.Images[i].URL;
-image.alt =json.Images[i].Caption;
-image.style = 'width:100%';
-image.class ="card-img-top";
-divCardBody.appendChild(caption);
-divCard.appendChild(image);
-divCard.appendChild(divCardBody);
-content.append(divCard);
-divCol.appendChild(divCard);
-gallery.appendChild(divCol);
-}
+	
+	var gallery = document.getElementById('gallery');
+
+
+
+	for(var i=0; i < json.Images.length; i++){
+		var img = document.createElement('img');
+		img.src = json.Images[i].URL;
+		img.alt = json.Images[i].Caption;
+		img.class = "card-img-top";
+		img.style = "width: 100%";
+
+		var aImg = document.createElement('a');
+		aImg.href = json.Images[i].URL;
+		aImg.setAttribute("data-bs-toggle","modal");
+		aImg.setAttribute("data-bs-toggle","modal");
+		aImg.setAttribute("data-bs-target","#myModal");
+		
+
+		var divCardBody = document.createElement('div');
+		divCardBody.class = "card-body";
+
+		var imgCaption = document.createElement('h4');
+		imgCaption.innerText = json.Images[i].Caption;
+		imgCaption.class = "card-title";
+		imgCaption.style = "text-align:center";
+
+		var divCard = document.createElement('div');
+		divCard.class = "card";
+		divCard.style = "width:100%"
+
+		var divCol = document.createElement('div');
+		divCol.class = "col";
+
+		aImg.appendChild(img);
+		divCardBody.appendChild(imgCaption);
+		divCard.appendChild(aImg);
+		divCard.appendChild(divCardBody);
+		divCol.appendChild(divCard);
+		gallery.appendChild(divCol);
+
+		//Carousel Button
+		var divButton = document.getElementById('divButton');
+		var cButton = document.createElement('button');
+		cButton.type ='button';
+		cButton.setAttribute("data-bs-target","#demo");
+		cButton.setAttribute("data-bs-slide-to",i);
+		if(i==0){
+			cButton.setAttribute("class","active");;
+		}
+		divButton.appendChild(cButton);
+
+		//Carousel Slide
+		var divSlide = document.getElementById('divSlide');
+		var divImage = document.createElement('div');
+		
+		if(i==0){
+			divImage.setAttribute("class","carousel-item active");
+		}
+		else{
+			divImage.setAttribute("class","carousel-item");
+		}
+
+		var imgSlide = document.createElement('img');
+		imgSlide.style="width:100%";
+		imgSlide.src=json.Images[i].URL;
+		imgSlide.alt=json.Images[i].Caption;
+		imgSlide.class="d-block w-100";
+
+		var divCaption = document.createElement('div');
+		divCaption.setAttribute("class","carousel-caption");
+		var imgSlideCaption = document.createElement('h4');
+		imgSlideCaption.innerText= json.Images[i].Caption;
+		divCaption.appendChild(imgSlideCaption);
+		divImage.appendChild(imgSlide);
+		divImage.appendChild(divCaption);
+		divSlide.appendChild(divImage);
+	}
+   
+
+  });
+});
+
+
